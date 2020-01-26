bf24a58b5363ec1aaa15de9d86c8d9aac062475e
Onewkub <oldnew123@gmail.com>
2020-01-27 01:06:28 +0700

    First Commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public int number;
+
+    public void Print()
+    {
+        Debug.Log("Value : " + number);
+    }
+
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text number;
+    private void Start()
+    {
+        number.text = card.number.ToString();
+    }
+}
