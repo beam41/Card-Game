e19a5e8d47bf93f68bfcdf274f6e9d0217c3d758
Onewkub <oldnew123@gmail.com>
2020-01-28 22:36:13 +0700

    Fixing some Script

---
+++ Assets/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool PlayerTurn;
+    private bool EnemyTurn;
+}
