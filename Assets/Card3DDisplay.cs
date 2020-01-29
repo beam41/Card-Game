6bf2384b131a0ed54642b021cc110e775db1f668
Onewkub <oldnew123@gmail.com>
2020-01-30 01:05:45 +0700

    add card Model

---
+++ Assets/Card3DDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Card3DDisplay : MonoBehaviour
+{
+    public Card card;
+    public TextMeshProUGUI charge;
+    public TextMeshProUGUI fullName;
+    public TextMeshProUGUI symbol;
+
+    private void Update()
+    {
+        if (card.charge > 0) charge.text = "+" + card.charge.ToString();
+        else if (card.charge < 0) charge.text = card.charge.ToString();
+        else charge.text = "";
+        fullName.text = card.fullName.ToString();
+        symbol.text = card.symbol.ToString();
+    }
+
+}
