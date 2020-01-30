595fba67363f520e4b1d27762b470b08c20a992f
Onewkub <oldnew123@gmail.com>
2020-01-30 17:33:34 +0700

    add 3d Card Script

--- Assets/Scripts/Card/Card3DDisplay.cs
+++ Assets/Scripts/Card/Card3DDisplay.cs
@@ -11,25 +11,45 @@ public class Card3DDisplay : MonoBehaviour
     public TextMeshProUGUI frontNum;
     public TextMeshProUGUI backNum;
 
+    private string[] prefixNum = {"", "Mono", "Di", "Tri", "Tetra", "Penta", "Hexa", "Hepta", "Octa", "Nona", "Deca"};
+
     private void Start()
     {
-        
+        GenerateNameAndSymbol();
     }
 
     private void GenerateNameAndSymbol(){
         var atomList = new Dictionary<string, int>();
-
+        int positiveCharge = 0;
+        int negativeCharge = 0;
+        Card positiveAtom = null;
+        Card negativeAtom = null;
 
         foreach(Card card in cards){
-            if(atomList.ContainsKey(card.name)){
-                atomList[card.name]++;
+            if(card.charge > 0){
+                positiveCharge++;
+                positiveAtom = card;
             }
-            else{
-                atomList[card.name] = 1;
+            else if(card.charge < 0){
+                negativeCharge++;
+                negativeAtom = card;
             }
         }
+        symbol.text = positiveAtom.symbol.ToString();
+        symbol_.text = negativeAtom.symbol.ToString();
+        // fullName.text =  positiveAtom.fullName.ToString();
 
-        
+        fullName.text = "";
+
+        if(positiveCharge == 1) fullName.text += positiveAtom.fullName;
+        else fullName.text += prefixNum[positiveCharge] + positiveAtom.fullName;
+        if(negativeCharge == 1) fullName.text += negativeAtom.negativeName;
+        else fullName.text += prefixNum[negativeCharge] + negativeAtom.negativeName;
+
+        symbol.text = positiveAtom.symbol;
+        symbol_.text = negativeAtom.symbol;
+        frontNum.text = positiveCharge.ToString();
+        backNum.text = negativeCharge.ToString();
     }
 
 }
