da721d721a987b3a7c634fb3b6143b20ac857b85
Onewkub <oldnew123@gmail.com>
2020-02-02 23:59:07 +0700

    add GameStart Scene

---
+++ Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneScript : MonoBehaviour
+{
+
+    public GameObject loginPage;
+    public GameObject registerPage;
+
+    public void openLoginPage()
+    {
+        registerPage.SetActive(false);
+        loginPage.SetActive(true);
+    }
+
+    public void openRegisterPage()
+    {
+        registerPage.SetActive(true);
+        loginPage.SetActive(false);
+    }
+}
